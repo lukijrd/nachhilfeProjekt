4a2fa310a4eb1e1749496dd78cdefb1d6eb72999
Darius Manita <darius-raul.manita@hakju.at>
2026-05-27 14:09:40 +0200

    ok

--- nachhilfe.py
+++ nachhilfe.py
@@ -1,14 +1,107 @@
 from flask import Flask, request, jsonify
 from supabase import create_client, Client
+import sqlite3
 
 app = Flask(__name__)
 
-SUPABASE_URL= "https://mtjpvnnqmnehapcvifdv.supabase.co"
-SUPABASE_KEY= "sb_publishable_db9KPKVXQn6IB5uqAas28Q_98UA5fXn"
+url = "https://mtjpvnnqmnehapcvifdv.supabase.co"
+publishable_key = "sb_publishable_db9KPKVXQn6IB5uqAas28Q_98UA5fXn"
 
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = create_client(url, publishable_key)
+
+conn = sqlite3.connect('faecher.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS faecher
+             (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)''')
+conn.commit()
+conn.close()
+
+faecher = []
+
+class Faecher:
+    def __init__(self, name):
+        self.name = name
+        conn = sqlite3.connect('faecher.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO faecher (name) VALUES (?)", (name,))
+        conn.commit()
+        conn.close()
+
+@app.route('/show_faecher', methods=['GET'])
+def show_faecher():
+    conn = sqlite3.connect('faecher.db')
+    c = conn.cursor()
+    c.execute("SELECT name FROM faecher")
+    faecher = c.fetchall()
+    conn.close()
+    return {'faecher': [f[0] for f in faecher]}
+
+@app.route('/add_fach', methods=['POST'])
+def add_fach():
+
+    data = request.get_json(force=True, silent=True)
+
+    print(data)   # DEBUG
+
+    if data is None or not isinstance(data, dict):
+        return {'message': 'Invalid JSON received'}, 400
+
+    name = data.get('name')
+
+    if not name:
+        return {'message': 'Name is required'}, 400
+
+    Faecher(name)
+
+    return {'message': 'Fach added successfully'}, 201
+
+@app.route('/update_fach/<int:id>', methods=['PUT'])
+def update_fach(id):
+
+    data = request.get_json()
+
+    if not data:
+        return {'message': 'No JSON data provided'}, 400
+
+    new_name = data.get('name')
+
+    if not new_name:
+        return {'message': 'New name is required'}, 400
+
+    conn = sqlite3.connect('faecher.db')
+    c = conn.cursor()
+
+    c.execute(
+        "UPDATE faecher SET name = ? WHERE id = ?",
+        (new_name, id)
+    )
+
+    conn.commit()
+
+    if c.rowcount == 0:
+        conn.close()
+        return {'message': 'Fach not found'}, 404
+
+    conn.close()
+
+    return {'message': 'Fach updated successfully'}
 
 
+@app.route('/delete_fach/<int:id>', methods=['DELETE'])
+def delete_fach(id):
+    conn = sqlite3.connect('faecher.db')
+    c = conn.cursor()
+
+    c.execute(
+        "DELETE FROM faecher WHERE id = ?",
+        (id,)
+    )
+    conn.commit()
+    if c.rowcount == 0:
+        conn.close()
+        return {'message': 'Fach not found'}, 404
+    conn.close()
+    return {'message': 'Fach deleted successfully'}
 
 
 if __name__ == '__main__':
